63c7cb9b74b0e72560748f42bf9609f1da1961b7
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-17 20:50:23 +0530

    add User entity class

---
+++ src/main/java/org/zr/tickets/domain/User.java
@@ -0,0 +1,41 @@
+package org.zr.tickets.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @Column(name = "id", updatable = false, nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    // TODO: Organized events
+
+    // TODO: Attending events
+
+    // TODO: Staffing events
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
