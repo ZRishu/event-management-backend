eeb5b2914fa0b81064d3c8aadc4b060129fc0109
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-18 19:22:14 +0530

    add CreateEventResponseDto

---
+++ src/main/kotlin/org/zr/tickets/domain/dtos/CreateEventResponseDto.kt
@@ -0,0 +1,19 @@
+package org.zr.tickets.domain.dtos
+
+import org.zr.tickets.domain.enums.EventStatusEnum
+import java.time.LocalDateTime
+import java.util.UUID
+
+data class CreateEventResponseDto(
+    val id: UUID,
+    val name: String,
+    val start: LocalDateTime?,
+    val end: LocalDateTime?,
+    val venue: String,
+    val salesStart: LocalDateTime?,
+    val salesEnd: LocalDateTime?,
+    val status: EventStatusEnum,
+    val ticketTypes: List<CreateTicketTypeResponseDto>,
+    val createdAt: LocalDateTime,
+    val updatedAt: LocalDateTime
+)
