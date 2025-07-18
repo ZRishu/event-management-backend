212c9627993cf5263a2803fb4e0c4182ecfc4cea
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-18 19:21:16 +0530

    fix: better null checking

--- src/main/kotlin/org/zr/tickets/domain/CreateEventRequest.kt
+++ src/main/kotlin/org/zr/tickets/domain/CreateEventRequest.kt
@@ -1,16 +1,15 @@
 package org.zr.tickets.domain
 
-import org.zr.tickets.domain.entities.User
 import org.zr.tickets.domain.enums.EventStatusEnum
 import java.time.LocalDateTime
 
 data class CreateEventRequest(
     val name: String,
-    val start: LocalDateTime,
-    val end: LocalDateTime,
+    val start: LocalDateTime?,
+    val end: LocalDateTime?,
     val venue: String,
-    val salesStart: LocalDateTime,
-    val salesEnd: LocalDateTime,
+    val salesStart: LocalDateTime?,
+    val salesEnd: LocalDateTime?,
     val status: EventStatusEnum,
     val ticketTypes: List<CreateTicketTypeRequest> = emptyList()
 )
--- src/main/kotlin/org/zr/tickets/domain/CreateTicketTypeRequest.kt
+++ src/main/kotlin/org/zr/tickets/domain/CreateTicketTypeRequest.kt
@@ -3,6 +3,6 @@ package org.zr.tickets.domain
 data class CreateTicketTypeRequest(
     val name: String,
     val price: Double,
-    val description: String,
-    val totalAvailable: Int
+    val description: String?,
+    val totalAvailable: Int?
 )
