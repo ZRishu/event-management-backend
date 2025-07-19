2d295c7e282adda9578d8f3198aeb73e1b213e6c
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-19 19:39:31 +0530

    add method to list events

--- src/main/java/org/zr/tickets/services/EventService.java
+++ src/main/java/org/zr/tickets/services/EventService.java
@@ -1,5 +1,7 @@
 package org.zr.tickets.services;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.zr.tickets.domain.CreateEventRequest;
 import org.zr.tickets.entities.Event;
 
@@ -7,4 +9,5 @@ import java.util.UUID;
 
 public interface EventService {
     Event createEvent(UUID organizerId, CreateEventRequest event);
+    Page<Event> listEventsForOrganizer(UUID organizerId, Pageable pageable);
 }
