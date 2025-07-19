e48e4ffdc8d86acb5a04c291feaea6a192ebcacd
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-20 00:32:12 +0530

    add custom method to get page of Events by their status

--- src/main/java/org/zr/tickets/repositories/EventRepository.java
+++ src/main/java/org/zr/tickets/repositories/EventRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.zr.tickets.domain.enums.EventStatusEnum;
 import org.zr.tickets.entities.Event;
 
 import java.util.Optional;
@@ -14,4 +15,6 @@ public interface EventRepository extends JpaRepository<Event, UUID> {
     Page<Event> findByOrganizerId(UUID organizerId, Pageable pageable);
 
     Optional<Event> findByIdAndOrganizerId(UUID id, UUID organizerId);
+
+    Page<Event> findByStatus(EventStatusEnum status, Pageable pageable);
 }
