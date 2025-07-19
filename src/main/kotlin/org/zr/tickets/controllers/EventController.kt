f47733f0c362add31fe78ef39125463aaeac866b
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-19 19:41:19 +0530

    add a get method list events by organizer Id

--- src/main/kotlin/org/zr/tickets/controllers/EventController.kt
+++ src/main/kotlin/org/zr/tickets/controllers/EventController.kt
@@ -1,19 +1,19 @@
 package org.zr.tickets.controllers
 
 import jakarta.validation.Valid
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.oauth2.jwt.Jwt
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.zr.tickets.domain.dtos.CreateEventRequestDto
 import org.zr.tickets.domain.dtos.CreateEventResponseDto
+import org.zr.tickets.domain.dtos.ListEventResponseDto
 import org.zr.tickets.mappers.EventMapper
 import org.zr.tickets.services.EventService
-import java.util.UUID
+import java.util.*
 
 @RestController
 @RequestMapping("/api/v1/events")
@@ -29,10 +29,23 @@ class EventController(
     ): ResponseEntity<CreateEventResponseDto> {
 
         val createEventRequest = eventMapper.fromDto(createEventRequestDto)
-        val userId = UUID.fromString(jwt.subject)
 
-        val createdEvent = eventService.createEvent(userId, createEventRequest)
+        val createdEvent = eventService.createEvent(parseUserId(jwt), createEventRequest)
         val eventDto = eventMapper.toDto(createdEvent)
         return ResponseEntity(eventDto, HttpStatus.CREATED)
+    }
+
+    @GetMapping
+    fun listEvents(
+        @AuthenticationPrincipal jwt: Jwt,
+        pageable: Pageable
+    ): ResponseEntity<Page<ListEventResponseDto>> {
+
+        val events = eventService.listEventsForOrganizer(parseUserId(jwt), pageable)
+        return ResponseEntity.ok(events.map {eventMapper.toListEventDto(it) })
+    }
+
+    private fun parseUserId(jwt: Jwt): UUID {
+        return UUID.fromString(jwt.subject)
     }
 }
