5a1204784a456f37c0adae4eaf438e96b02af8a0
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-19 19:39:59 +0530

    add implementation of method to list events

--- src/main/java/org/zr/tickets/services/Impl/EventServiceImpl.java
+++ src/main/java/org/zr/tickets/services/Impl/EventServiceImpl.java
@@ -1,6 +1,8 @@
 package org.zr.tickets.services.Impl;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.zr.tickets.domain.CreateEventRequest;
 import org.zr.tickets.entities.Event;
@@ -53,5 +55,10 @@ public class EventServiceImpl implements EventService {
         eventToCreate.setTicketTypes(ticketTypesToCreate);
 
         return eventRepository.save(eventToCreate);
+    }
+
+    @Override
+    public Page<Event> listEventsForOrganizer(UUID organizerId, Pageable pageable) {
+        return eventRepository.findByOrganizerId(organizerId, pageable);
     }
 }
