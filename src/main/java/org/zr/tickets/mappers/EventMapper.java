dacdc673564fa31e584e7d671aadbbf9f474cdd1
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-19 19:40:48 +0530

    map events to List events Dto

--- src/main/java/org/zr/tickets/mappers/EventMapper.java
+++ src/main/java/org/zr/tickets/mappers/EventMapper.java
@@ -4,10 +4,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import org.zr.tickets.domain.CreateEventRequest;
 import org.zr.tickets.domain.CreateTicketTypeRequest;
-import org.zr.tickets.domain.dtos.CreateEventRequestDto;
-import org.zr.tickets.domain.dtos.CreateEventResponseDto;
-import org.zr.tickets.domain.dtos.CreateTicketTypeRequestDto;
-import org.zr.tickets.domain.dtos.CreateTicketTypeResponseDto;
+import org.zr.tickets.domain.dtos.*;
 import org.zr.tickets.entities.Event;
 import org.zr.tickets.entities.TicketType;
 
@@ -17,7 +14,9 @@ public interface EventMapper {
 
     CreateEventRequest fromDto(CreateEventRequestDto dto);
 
-    CreateTicketTypeResponseDto toDto(TicketType ticketType);
-
     CreateEventResponseDto toDto(Event event);
+
+    ListEventTicketTypeResponseDto toDto(TicketType ticketType);
+
+    ListEventResponseDto toListEventDto(Event event);
 }
