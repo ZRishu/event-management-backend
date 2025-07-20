0b4275179a4a10769fc58e0cb225acacc3c00642
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-20 17:08:56 +0530

    add method in TicketRepository to list users tickets

--- src/main/java/org/zr/tickets/repositories/TicketRepository.java
+++ src/main/java/org/zr/tickets/repositories/TicketRepository.java
@@ -1,5 +1,7 @@
 package org.zr.tickets.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.zr.tickets.entities.Ticket;
@@ -9,4 +11,5 @@ import java.util.UUID;
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, UUID> {
     int countByTicketTypeId(UUID ticketTypeId);
+    Page<Ticket> findByPurchaserId(UUID purchaserId, Pageable pageable);
 }
