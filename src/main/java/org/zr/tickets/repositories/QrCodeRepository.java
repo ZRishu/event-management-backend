84bcb7beee1c0f9e8f6b11de542aa813d02ae4e1
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-20 19:04:03 +0530

    add a custom method in QrCodeRepository to get QR Code by ticketId and UserId

--- src/main/java/org/zr/tickets/repositories/QrCodeRepository.java
+++ src/main/java/org/zr/tickets/repositories/QrCodeRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.zr.tickets.entities.QrCode;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface QrCodeRepository extends JpaRepository<QrCode, UUID> {
+    Optional<QrCode> findByTicketIdAndTicketPurchaserId(UUID ticketId, UUID ticketPurchaserId);
 }
