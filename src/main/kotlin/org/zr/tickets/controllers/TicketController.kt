2e1f8b364f5e53f41a72cb85e4c568623672163d
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-20 18:35:57 +0530

    add GET method in TicketController to get single Ticket

--- src/main/kotlin/org/zr/tickets/controllers/TicketController.kt
+++ src/main/kotlin/org/zr/tickets/controllers/TicketController.kt
@@ -2,15 +2,19 @@ package org.zr.tickets.controllers
 
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.zr.tickets.domain.dtos.GetTicketResponseDto
 import org.zr.tickets.domain.dtos.ListTicketResponseDto
 import org.zr.tickets.mappers.TicketMapper
 import org.zr.tickets.services.TicketService
 import org.zr.tickets.util.JwtUtil.parseUserId
+import java.util.UUID
 
 @RestController
 @RequestMapping("/api/v1/tickets")
@@ -26,5 +30,17 @@ class TicketController(
 
         return ticketService.listTicketForUser(parseUserId(jwt), pageable)
             .map { ticketMapper.toDto(it) }
+    }
+
+    @GetMapping("/{ticketId}")
+    fun getTicket(
+        @AuthenticationPrincipal jwt: Jwt,
+        @PathVariable ticketId: UUID,
+        ): ResponseEntity<GetTicketResponseDto> {
+
+        return ticketService.getTicketForUser(parseUserId(jwt), ticketId)
+            .map { ticketMapper.toGetTicketResponseDto(it) }
+            .map { ResponseEntity.ok(it) }
+            .orElse(ResponseEntity.notFound().build())
     }
 }
