6c9d51866c0e4ddee2756af78eb185c0b5a0e458
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-20 18:24:16 +0530

    map GetTicketResponseDto to Ticket, TicketType and Event info

--- src/main/java/org/zr/tickets/mappers/TicketMapper.java
+++ src/main/java/org/zr/tickets/mappers/TicketMapper.java
@@ -1,7 +1,9 @@
 package org.zr.tickets.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
+import org.zr.tickets.domain.dtos.GetTicketResponseDto;
 import org.zr.tickets.domain.dtos.ListTicketResponseDto;
 import org.zr.tickets.domain.dtos.ListTicketTicketTypeResponseDto;
 import org.zr.tickets.entities.Ticket;
@@ -12,4 +14,12 @@ public interface TicketMapper {
     ListTicketTicketTypeResponseDto toDto(TicketType ticketType);
 
     ListTicketResponseDto toDto(Ticket ticket);
+
+    @Mapping(target = "price", source = "ticket.ticketType.price")
+    @Mapping(target = "description", source = "ticket.ticketType.description")
+    @Mapping(target = "eventName", source = "ticket.ticketType.event.name")
+    @Mapping(target = "eventVenue", source = "ticket.ticketType.event.venue")
+    @Mapping(target = "eventStart", source = "ticket.ticketType.event.start")
+    @Mapping(target = "eventEnd", source = "ticket.ticketType.event.end")
+    GetTicketResponseDto toGetTicketResponseDto(Ticket ticket);
 }
