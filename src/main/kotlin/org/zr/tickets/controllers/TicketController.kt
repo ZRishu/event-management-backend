501e36e052497ddca3c5fa2b87b4c3120b76fdff
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-20 19:05:12 +0530

    add GET method to get QR Code in TicketController

--- src/main/kotlin/org/zr/tickets/controllers/TicketController.kt
+++ src/main/kotlin/org/zr/tickets/controllers/TicketController.kt
@@ -2,6 +2,7 @@ package org.zr.tickets.controllers
 
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.oauth2.jwt.Jwt
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController
 import org.zr.tickets.domain.dtos.GetTicketResponseDto
 import org.zr.tickets.domain.dtos.ListTicketResponseDto
 import org.zr.tickets.mappers.TicketMapper
+import org.zr.tickets.services.QrCodeService
 import org.zr.tickets.services.TicketService
 import org.zr.tickets.util.JwtUtil.parseUserId
 import java.util.UUID
@@ -20,7 +22,8 @@ import java.util.UUID
 @RequestMapping("/api/v1/tickets")
 class TicketController(
     private val ticketService: TicketService,
-    private val ticketMapper: TicketMapper
+    private val ticketMapper: TicketMapper,
+    private val qrCodeService: QrCodeService
 ) {
     @GetMapping
     fun listTickets(
@@ -42,5 +45,19 @@ class TicketController(
             .map { ticketMapper.toGetTicketResponseDto(it) }
             .map { ResponseEntity.ok(it) }
             .orElse(ResponseEntity.notFound().build())
+    }
+
+    @GetMapping("/{ticketId}/qr-codes")
+    fun getTicketQrCode(
+        @AuthenticationPrincipal jwt: Jwt,
+        @PathVariable ticketId: UUID
+    ): ResponseEntity<ByteArray> {
+
+        val qrCodeImage = qrCodeService.getQrCodeImageForUserAndTicket(parseUserId(jwt), ticketId)
+
+        return ResponseEntity.ok()
+            .contentType(MediaType.IMAGE_PNG)
+            .contentLength(qrCodeImage.size.toLong())
+            .body(qrCodeImage)
     }
 }
