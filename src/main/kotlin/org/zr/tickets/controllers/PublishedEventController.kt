336017008cc0063f8a884c8d6fc32aeeea604013
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-20 09:11:14 +0530

    add logic for search with query parameter in GET method

--- src/main/kotlin/org/zr/tickets/controllers/PublishedEventController.kt
+++ src/main/kotlin/org/zr/tickets/controllers/PublishedEventController.kt
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.zr.tickets.domain.dtos.ListPublishedEventResponseDto
 import org.zr.tickets.mappers.EventMapper
@@ -19,12 +20,17 @@ class PublishedEventController(
 
     @GetMapping
     fun listPublishedEvents(
+        @RequestParam(required = false) q: String?,
         pageable: Pageable
     ): ResponseEntity<Page<ListPublishedEventResponseDto>> {
 
+        val events = if (!q.isNullOrBlank()) eventService.searchPublishedEvents(
+            q,
+            pageable
+        ) else eventService.listPublishedEvents(pageable)
+
         return ResponseEntity.ok(
-            eventService.listPublishedEvents(pageable)
-                .map { eventMapper.toListPublishedEventResponseDto(it) }
+            events.map { eventMapper.toListPublishedEventResponseDto(it) }
         )
     }
 }
